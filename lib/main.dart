537a4488750c6cefff3a792e942f6b4009ca49fa
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-19 18:59:15 +0530

    Commiting changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(),
       home: TasksScreen(),
     );
   }
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,6 +5,81 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<CheckboxListTile> todoList = [];
+
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent.shade200,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent.shade200,
+        onPressed: () {
+          todoList.add(
+            CheckboxListTile(
+              value: false,
+              onChanged: (value) {
+                value = true;
+              },
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlueAccent.shade200,
+                    size: 30,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  topLeft: Radius.circular(20),
+                ),
+              ),
+              child: ListView(
+                children: todoList,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
